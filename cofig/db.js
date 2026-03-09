ecd57f6d2542dbbba1670a4a31430b621156391f
LEGENDSASI <89404464+LEGENDSASI@users.noreply.github.com>
2026-03-09 22:35:41 +0530

    Update MongoDB connection URI in db.js
    Changed MongoDB connection URI to a hardcoded value.

--- cofig/db.js
+++ cofig/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect("mongodb+srv://prasad:prasad2005@cluster0.zd6zc.mongodb.net/pgs");
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
