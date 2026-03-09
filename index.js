e35183dd1bbee90145f3b60945935a1f1593fdde
LEGENDSASI <89404464+LEGENDSASI@users.noreply.github.com>
2026-03-09 22:27:49 +0530

    Enable CORS for all origins

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ const app = express();
 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
-
-const corsOptions = {
-  origin: 'http://localhost:5173',
-  optionsSuccessStatus: 200 
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.get("/average-reviews", async (req, res) => {
     try {
