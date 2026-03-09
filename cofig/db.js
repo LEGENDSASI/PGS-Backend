e848d0cb4bd4ad98e7e948077c1900c67fe1b257
LEGENDSASI <89404464+LEGENDSASI@users.noreply.github.com>
2026-03-09 22:47:17 +0530

    Update MongoDB connection string in db.js

--- cofig/db.js
+++ cofig/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connectDB() {
     try {
-        await mongoose.connect("mongodb+srv://prasad:prasad2005@cluster0.zd6zc.mongodb.net/pgs");
+        await mongoose.connect("mongodb+srv://romansasi63747-pgs:Romansasi63747%40@pgs.rx3tu78.mongodb.net/?appName=pgs");
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
